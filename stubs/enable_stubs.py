ec4a042309ebb4b98ea214e5818be8960322e916
Cody J. Phillips <codyp@cis.upenn.edu>
2020-02-18 14:12:02 -0500

    Add stubs and tests

---
+++ stubs/enable_stubs.py
@@ -0,0 +1,14 @@
+import sys
+from pathlib import Path
+
+class StubbedModule():
+    pass
+
+def get_stubs_directory():
+    return str(Path(__file__).parent)
+
+def enable_stubs():
+    import opensfm
+    opensfm.csfm = StubbedModule()
+    sys.modules["pyopengv"] = StubbedModule()
+    sys.path.insert(0, get_stubs_directory())
